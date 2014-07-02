375ce20ad7cd2fe1e76f550d670c0c87b67777a5
laonawuli <laonawuli@126.com>
2014-07-02 22:17:23 +0800

    Pages

--- js/classes/words.js
+++ js/classes/words.js
@@ -1,9 +1,9 @@
 function Words() {
     this.basic = "public class Basic\n" +
         "{\n" +
-        "    private String firstName = \"Xu\";" +
-        "    private String lastName = \"Zhang\";" +
-        "    private String sex = \"Male\";" +
+        "    private String firstName = \"Xu\";\n" +
+        "    private String lastName = \"Zhang\";\n" +
+        "    private String sex = \"Male\";\n" +
         "    private int age = 28;\n" +
         "    private String phone = \"+8615750481555\";\n" +
         "    private String email = \"laonawuli@gmail.com\";\n" +
