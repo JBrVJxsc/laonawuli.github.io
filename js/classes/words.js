bfe76f5bcb4a800f2548d54604e6ed20d925087b
laonawuli <laonawuli@126.com>
2014-07-02 22:11:57 +0800

    Pages

--- js/classes/words.js
+++ js/classes/words.js
@@ -1,8 +1,20 @@
 function Words() {
-    this.basic = "public class Xu\n" +
+    this.basic = "public class Basic\n" +
         "{\n" +
-        "    private age = 28;\n" +
-        "    private phone = \"15750481555\";\n" +
-        "    private email = \"laonawuli@126.com\";\n" +
+        "    private String firstName = \"Xu\";" +
+        "    private String lastName = \"Zhang\";" +
+        "    private String sex = \"Male\";" +
+        "    private int age = 28;\n" +
+        "    private String phone = \"+8615750481555\";\n" +
+        "    private String email = \"laonawuli@gmail.com\";\n" +
+        "}";
+
+    this.employment = "public class Employment\n" +
+        "{\n" +
+        "    private String position = \"\";\n" +
+        "    private String company = \"\";\n" +
+        "    private String dateFrom = \"\";\n" +
+        "    private String dateTo = \"\";\n" +
+        "    private String description = \"\";\n" +
         "}";
 }
