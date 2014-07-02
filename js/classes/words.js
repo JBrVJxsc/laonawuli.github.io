48dc482201cf54db6dff467e6309443923ab316d
laonawuli <laonawuli@126.com>
2014-07-02 21:51:04 +0800

    Pages

---
+++ js/classes/words.js
@@ -0,0 +1,8 @@
+function Words() {
+    this.basic = "public class Xu\n" +
+        "{\n" +
+        "    private age = 28;\n" +
+        "    private phone = 15750481555;\n" +
+        "    private email = laonawuli@126.com;\n" +
+        "}";
+}
