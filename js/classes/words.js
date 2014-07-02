ccf77f38e8fe7a54c37d002bffee082739e03d13
laonawuli <laonawuli@126.com>
2014-07-02 22:47:42 +0800

    Pages

--- js/classes/words.js
+++ js/classes/words.js
@@ -1,20 +1,41 @@
 function Words() {
-    this.basic = "public class Basic\n" +
-        "{\n" +
-        "    private String firstName = \"Xu\";\n" +
-        "    private String lastName = \"Zhang\";\n" +
-        "    private String sex = \"Male\";\n" +
-        "    private int age = 28;\n" +
-        "    private String phone = \"+8615750481555\";\n" +
-        "    private String email = \"laonawuli@gmail.com\";\n" +
-        "}";
+    this.package = "package me.xuzh.resume";
 
-    this.employment = "public class Employment\n" +
+    this.main = "public class Resume\n" +
         "{\n" +
-        "    private String position = \"\";\n" +
-        "    private String company = \"\";\n" +
-        "    private String dateFrom = \"\";\n" +
-        "    private String dateTo = \"\";\n" +
-        "    private String description = \"\";\n" +
-        "}";
+        "    public static void main(String[] args)\n" +
+        "    {\n\n" +
+        "    }";
+
+    this.basic = "    private class Basic\n" +
+        "    {\n" +
+        "        private String firstName = \"Xu\";\n" +
+        "        private String lastName = \"Zhang\";\n" +
+        "        private String sex = \"Male\";\n" +
+        "        private int age = 28;\n" +
+        "        private String phone = \"+8615750481555\";\n" +
+        "        private String email = \"laonawuli@gmail.com\";\n" +
+        "    }";
+
+    this.employment = "    private class Employment\n" +
+        "    {\n" +
+        "        private String position = \"\";\n" +
+        "        private String company = \"\";\n" +
+        "        private String dateFrom = \"\";\n" +
+        "        private String dateTo = \"\";\n" +
+        "        private String description = \"\";\n" +
+        "    }";
+
+    this.project = "    private class Project\n" +
+        "    {\n" +
+        "        private String name = \"\";\n" +
+        "        private String url = \"\";\n" +
+        "        private String demo = \"\";\n" +
+        "    }";
+
+    this.skill = "    private class Skill\n" +
+        "    {\n" +
+        "        private String name = \"\";\n" +
+        "        private String level = \"\";\n" +
+        "    }";
 }
